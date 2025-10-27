dd0c4061dab89a3ce667f76b49f5869e4d8caa8b
John Marlo Lapiz <91590512+lazuee@users.noreply.github.com>
2025-10-28 05:17:31 +0800

    Added getProcesses for macos

--- src/process/native/index.ts
+++ src/process/native/index.ts
@@ -3,3 +3,5 @@ export const linux =
 	process.platform === "linux" ? await import("./linux") : undefined;
 export const win32 =
 	process.platform === "win32" ? await import("./win32") : undefined;
+export const darwin =
+	process.platform === "darwin" ? await import("./darwin") : undefined;
